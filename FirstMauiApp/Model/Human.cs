2769febb0020efd2870848a5d8aefb06ff0d1ff4
Sharap <toptricker8@gmail.com>
2023-05-19 10:35:45 +0300

    -Delete unnessecary fields -CodeStyle changes

--- FirstMauiApp/Model/Human.cs
+++ FirstMauiApp/Model/Human.cs
@@ -2,19 +2,17 @@
 {
     public class Human
     {
-        private string firstName;
-        private string secondName;
-        private DateTime birthDate;
 
-        public string FirstName { get { return firstName; } set { firstName = value; } }
-        public string SecondName { get { return secondName; } set { secondName = value; } }
-        public DateTime BirthDate { get { return birthDate; } set { birthDate = value; } }
 
-        public Human(String _firstName, String _secondName, DateTime _birthDate)
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Human(string firstName, string secondName, DateTime birthDate)
         {
-            firstName = _firstName;
-            secondName = _secondName;
-            birthDate = _birthDate;
+            FirstName = firstName;
+            SecondName = secondName;
+            BirthDate = birthDate;
         }
     }
 }
